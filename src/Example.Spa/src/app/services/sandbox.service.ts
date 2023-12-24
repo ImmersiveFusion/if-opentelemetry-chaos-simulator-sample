8e13e89976f474049cc126cef6a28acc8093fc29
gmneon <neon@gm.immersivefusion.net>
2023-12-24 14:27:09 -0500

    ng update @angular/cli@16

--- src/Example.Spa/src/app/services/sandbox.service.ts
+++ src/Example.Spa/src/app/services/sandbox.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class SandboxService {
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
 
   close(resource: string) {
    
