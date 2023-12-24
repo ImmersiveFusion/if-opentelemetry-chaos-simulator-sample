176423fb9cd69f8f92bead640af0a40870a81ee3
gmneon <neon@gm.immersivefusion.net>
2023-12-24 14:25:51 -0500

    Api fixes.

--- src/Example.Spa/src/environments/environment.development.ts
+++ src/Example.Spa/src/environments/environment.development.ts
@@ -1,1 +1,3 @@
-export const environment = {};
+export const environment = {
+    apiUri: 'https://localhost:7263'
+};
--- src/Example.Spa/src/environments/environment.ts
+++ src/Example.Spa/src/environments/environment.ts
@@ -1,1 +1,3 @@
-export const environment = {};
+export const environment = {
+    apiUri: `${window.location.protocol}://${window.location.host}`
+};
