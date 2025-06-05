41ae62a237a9804936d39dd57ff6d137f70b2ce5
Dan Kowalski <dkowalski@immersivefusion.com>
2025-06-04 21:49:25 -0400

    Angular 19.

--- src/Example.Spa/src/app/pipes/replace-line-breaks.pipe.ts
+++ src/Example.Spa/src/app/pipes/replace-line-breaks.pipe.ts
@@ -1,5 +1,8 @@
 import { Pipe, PipeTransform } from '@angular/core';
-@Pipe({name: 'rn2br'})
+@Pipe({
+    name: 'rn2br',
+    standalone: false
+})
 export class ReplaceLineBreaksPipe implements PipeTransform {
 transform(value: string): string {
       return value.replace(/\\r\\n/g, '<br/>');
